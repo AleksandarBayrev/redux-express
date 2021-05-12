800caa5d6f1c9ef9367c12a51f49c9d467a0ee02
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2021-05-12 12:16:16 +0300

    Update logic for UI redux element

--- public/javascripts/ui/ui.js
+++ public/javascripts/ui/ui.js
@@ -1,10 +1,16 @@
 const UI = (function() {
+    const shouldUpdateElement = (element, dataAsJson) => element && element.innerHTML !== dataAsJson
+
+    const updateElement = (element, data) => {
+        const dataAsJson = JSON.stringify(data)
+        if (shouldUpdateElement(element, dataAsJson)) {
+            element.innerHTML = dataAsJson
+        }
+    }
+
     const updateData = (data) => {
         const element = document.getElementById('data')
-        const dataAsJson = JSON.stringify(data)
-        if (element.innerHTML !== dataAsJson) {
-            document.getElementById('data').innerHTML = JSON.stringify(data)
-        }
+        updateElement(element, data)
     }
     return {
         updateData
